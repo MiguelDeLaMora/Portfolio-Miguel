fc9e3cf7ce437dab7987652138073f8d97196e90
Miguel De La Mora <migueldelamora@Miguels-Mac-mini.local>
2024-08-13 17:57:43 -0600

    Adding Hero Text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,34 @@ const hero = () => {
   return (
     <div>
         <div className='bg-white max-w-[1200px] h-[80vh] mx-auto flex flex-col-reverse sm:flex-row justify-center align-center'>
+
+            <div className='bg-blue-300 flex-col my-auto mx-auto md:mx-0'>
+                <p className='md:text-5xl sm:text-4xl text-xl font-bold text-gray-200'>Hi, I am Miguel de la Mora</p>
+                <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'><TypeAnimation
+                sequence={[
+                    "Frontend Dev",
+                    1000,
+                    "Webdesigner",
+                    1000,
+                    "Industrialdesigner",
+                    1000,
+                ]}
+                wrapper='span'
+                speed={50}
+                repeat={Infinity}
+                />
+                </h1>
+
+            </div>
+            
             <div className='text-8xl'>
                 text
             </div>
+            
             <div className='my-auto'>
-                <img className='w-[300px] sm:w-[500px] mx-auto h-auto rounded-full' src={profilepic} alt="profile pic" />
+                <img className='w-[300px] sm:w-[500px] mx-auto h-auto' src={profilepic} alt="profile pic" />
             </div>
+        
         </div>
     </div>
   )
